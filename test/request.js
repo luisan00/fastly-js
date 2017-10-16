559533fb1c19a277297a442a728bfc4ad6825d09
luisan00 <luisan00@hotmail.com>
2017-10-16 23:14:52 +0200

    Update request.js

--- test/request.js
+++ test/request.js
@@ -23,6 +23,38 @@ tap.test('request GET -> then()', (t) => {
 });
 // --> .request.send() => .catch(object)
 tap.test('request GET -> catch()', (t) => {
+    flib.request.options.method = 'GET';
+    var wrong_url = 'sorry for the inconveniences, im testing a new library :(';
+    flib.request.options.path = `/content/edge_check?url=${wrong_url}`;
+    flib.request.send()
+        .then((res) => {
+            t.fail(res);
+            t.end()
+        })
+        .catch((err) => {
+            t.type(err, 'object', '.catch(object)');
+            t.end();
+        })
+});
+
+// POST
+// --> .request.send(< GET >) => .then(object)
+tap.test('request GET -> then()', (t) => {
+    t.type(flib.request, 'object', 'request => object');
+    flib.request.options.method = 'POST';
+    flib.request.options.path = `/content/edge_check?url=${content_url}`;
+    flib.request.send()
+        .then((res) => {
+            t.type(res, 'object', '.then(object)');
+            t.end();
+        })
+        .catch((err) => {
+            t.fail(err);
+            t.end();
+        })
+});
+// --> .request.send() => .catch(object)
+tap.test('request GET -> catch()', (t) => {
     flib.request.options.method = 'POST';
     var wrong_url = 'sorry for the inconveniences, im testing a new library :(';
     flib.request.options.path = `/content/edge_check?url=${wrong_url}`;
