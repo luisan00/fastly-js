389ce4649f96856c63ef832851e1901a90eb90dd
luisan00 <luisan00@hotmail.com>
2017-10-16 23:48:20 +0200

    Update request.js

--- test/request.js
+++ test/request.js
@@ -1,67 +1,24 @@
 const tap = require('tap');
 const fastly = require('../lib/fastly.js');
-const content_url = process.env.CONTENT_URL || '';
 const service_id = process.env.SERVICE_ID || '';
 const fastly_api_key = process.env.FASTLY_API_KEY || '';
-
+//
+const content_url = process.env.CONTENT_URL || '';
+const wrong_url = 'wrong_url';
+//
 var flib = new fastly(fastly_api_key);
-
-tap.test('GET => .then', (t) => {
-    t.type(flib.request, 'object', 'request => object');
-    flib.request.options.method = 'GET';
-    flib.request.options.path = `/content/edge_check?url=${content_url}`;
-    flib.request.send()
-        .then((res) => {
-            t.type(res, 'object', '.then()');
-            t.end();
-        })
-        .catch((err) => {
-            t.fail(err);
-            t.end();
-        })
-});
-
-tap.test('GET => .catch', (t) => {
-    flib.request.options.method = 'GET';
-    var wrong_url = 'sorry for the inconveniences, im testing a new library :(';
-    flib.request.options.path = `/content/edge_check?url=${wrong_url}`;
-    flib.request.send()
-        .then((res) => {
-            t.fail(res);
-            t.end()
-        })
-        .catch((err) => {
-            t.type(err, 'object', '.catch()');
-            t.end();
-        })
-});
-
-tap.test('POST => .then', (t) => {
+tap.test('POST will return string', (t) => {
     t.type(flib.request, 'object', 'request => object');
     flib.request.options.method = 'POST';
     flib.request.options.path = `/content/edge_check?url=${content_url}`;
     flib.request.send()
         .then((res) => {
-            t.type(res, 'string', '.then()');
+            console.log(res)
+            t.type(res, 'string', 'string');
             t.end();
         })
         .catch((err) => {
             t.fail(err);
-            t.end();
-        })
-});
-
-tap.test('POST => .catch', (t) => {
-    flib.request.options.method = 'POST';
-    var wrong_url = 'sorry for the inconveniences, im testing a new library :(';
-    flib.request.options.path = `/content/edge_check?url=${wrong_url}`;
-    flib.request.send()
-        .then((res) => {
-            t.fail(res);
-            t.end()
-        })
-        .catch((err) => {
-            t.type(err, 'object', '.catch()');
             t.end();
         })
 });
