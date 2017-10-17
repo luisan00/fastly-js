eaece341828a2231220dd056473b321b650962e8
luisan00 <luisan00@hotmail.com>
2017-10-17 23:52:13 +0200

    Update healthcheck.js

--- test/healthcheck.js
+++ test/healthcheck.js
@@ -1,1 +1,7 @@
-//
+const tap = require('tap');
+const fastly = require('../lib/fastly.js');
+const fastly_api_key =  process.env.FASTLY_API_KEY || '';
+const content_url =  process.env.CONTENT_URL || '';
+
+var flib = new fastly(fastly_api_key);
+
